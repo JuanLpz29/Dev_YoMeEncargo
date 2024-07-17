c65f0c0ed24326454ddd8a75488903c30243133f
JuanLpz29 <juanlpz29@outlook.com>
2024-07-17 04:08:53 -0400

    Update del header

--- YoMeEncargo/src/App.jsx
+++ YoMeEncargo/src/App.jsx
@@ -16,10 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Esto es Yo me Encargo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Has presionado el botón {count} veces
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
